8bbb15d6d4cec0d84f029ad98232afc6a1060961
Vadim Surin <7uperior@gmail.com>
2024-01-15 12:28:04 +0300

    Create database connection for API

--- todo_app/database.py
+++ todo_app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_threas": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLoacal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
