8bed10125ce56ce15152d672876b0bfc0355073b
Vadim Surin <7uperior@gmail.com>
2024-01-15 15:48:50 +0300

    Add Flake exception

--- todo_app/main.py
+++ todo_app/main.py
@@ -23,7 +23,7 @@ async def read_all(db: Session = Depends(get_db)):
 
 @app.get("/todo/{todo_id}")
 async def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()  # noqa: E501
     if todo_model is not None:
         return todo_model
     raise http_exception()
